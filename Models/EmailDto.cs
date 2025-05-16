321afda2a869264297daafcef060a80b5d19cd70
educalok <eduardo.calzone@gmail.com>
2025-05-16 23:54:44 +0200

    Add project files.

---
+++ Models/EmailDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmailSenderAPI.Models
+{
+    public class EmailDto
+    {
+        [Required]
+        public string To { get; set; } = string.Empty;
+        [Required]
+        public string ContactName { get; set; } = string.Empty;
+        [Required]
+        public string Body { get; set; } = string.Empty;
+    }
+}
---
+++ Services/EmailService/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using EmailSenderAPI.Models;
+
+namespace EmailSenderAPI.Services.EmailService
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
